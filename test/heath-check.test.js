19f02033a4d2e9d848d1546ac97580f1a461fddd
Endryus <endryush@gmail.com>
2024-04-17 23:47:17 -0300

    removing mock and create workflow

--- test/heath-check.test.js
+++ test/heath-check.test.js
@@ -1,12 +1,10 @@
 import request from 'supertest';
 import app from '../src/server'
-import '../src/config/dotenv'
 
 describe('Testing API on /health-check', () => {
   test('get / health-check', async () => {
     const res = await request(app)
       .get('/api/health-check')
-      .auth(process.env.JEST_USR, process.env.JEST_PASS)
 
       expect(res.status).toBe(200)
   })
